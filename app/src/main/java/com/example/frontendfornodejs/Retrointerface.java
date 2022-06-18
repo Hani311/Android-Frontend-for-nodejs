f866887f2d5dbe20d2e2d3282d4a761b71a6c88c
Hani <57943681+Hani311@users.noreply.github.com>
2022-06-18 22:18:46 +0200

    Initial commit

---
+++ app/src/main/java/com/example/frontendfornodejs/Retrointerface.java
@@ -0,0 +1,16 @@
+package com.example.frontendfornodejs;
+import java.util.HashMap;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+interface RetrofitInterface {
+
+    @POST("/login")
+    Call<LoginResult> executeLogin(@Body HashMap<String, String> map);
+
+    @POST("/signup")
+    Call<Void> executeSignup (@Body HashMap<String, String> map);
+
+}
